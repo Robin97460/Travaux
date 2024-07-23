9a6b744bc69215e32e25717d813a70c20099c727
ALLEMAN_Robin <robin974robin@gmail.com>
2024-07-23 03:39:50 +0200

    fin du cours 5 sur js et du projet sur l'appli de yoga

--- JavaScript/Cours/Cours5/yoga-routine/index.js
+++ JavaScript/Cours/Cours5/yoga-routine/index.js
@@ -1,6 +1,6 @@
 const main = document.querySelector("main");
 
-let exerciceArray = [
+const basicArray = [
     { pic: 0, min: 1 },
     { pic: 1, min: 1 },
     { pic: 2, min: 1 },
@@ -13,13 +13,135 @@ let exerciceArray = [
     { pic: 9, min: 1 },
 ];
 
-class Exercice {}
+let exerciceArray = [];
+
+//__________________________
+//Get stored exercices array
+(() => {
+    if (localStorage.exercices) {
+        exerciceArray = JSON.parse(localStorage.exercices);
+    } else {
+        exerciceArray = basicArray;
+    }
+})();
+//_________________________
+class Exercice {
+    constructor() {
+        this.index = 0;
+        this.minutes = exerciceArray[this.index].min;
+        this.seconds = 0;
+    }
+    updateCountdown() {
+        this.seconds = this.seconds < 10 ? "0" + this.seconds : this.seconds;
+
+        setTimeout(() => {
+            if (this.minutes === 0 && this.seconds === "00") {
+                this.index++;
+                this.ring();
+                if (this.index < exerciceArray.length) {
+                    this.minutes = exerciceArray[this.index].min;
+                    this.seconds = 0;
+                    this.updateCountdown();
+                } else {
+                    page.finish();
+                }
+            } else if (this.seconds === "00") {
+                this.minutes--;
+                this.seconds = 59;
+                this.updateCountdown();
+            } else {
+                this.seconds--;
+                this.updateCountdown();
+            }
+        }, 10);
+
+        return (main.innerHTML = `
+            <div class="exercice-container">
+            <p>${this.minutes}:${this.seconds}</p>
+            <img src="./img/${exerciceArray[this.index].pic}.png">
+            <div>${this.index + 1}/${exerciceArray.length}</div>
+            
+            </div>
+            `);
+    }
+    ring() {
+        const audio = new Audio();
+        audio.src = "ring.mp3";
+        audio.play();
+    }
+}
 
 const utils = {
     pageContent: function (title, content, btn) {
         document.querySelector("h1").innerHTML = title;
         main.innerHTML = content;
         document.querySelector(".btn-container").innerHTML = btn;
+    },
+
+    EventMinutes: function () {
+        document.body
+            .querySelectorAll("input[type='number']")
+            .forEach((input) => {
+                input.addEventListener("input", (e) => {
+                    exerciceArray.map((exo) => {
+                        if (exo.pic == e.target.id) {
+                            exo.min = parseInt(e.target.value);
+                            this.store();
+                        }
+                    });
+                });
+            });
+    },
+
+    EventArrow: function () {
+        document.querySelectorAll(".arrow").forEach((arrow) => {
+            arrow.addEventListener("click", (e) => {
+                let position = 0;
+                exerciceArray.map((exo) => {
+                    if (exo.pic == e.target.dataset.pic && position !== 0) {
+                        [exerciceArray[position], exerciceArray[position - 1]] =
+                            [
+                                exerciceArray[position - 1],
+                                exerciceArray[position],
+                            ];
+                        page.lobby();
+                        this.store();
+                    } else {
+                        position++;
+                    }
+                });
+            });
+        });
+    },
+    EventCross: function () {
+        document.querySelectorAll(".deleteBtn").forEach((cross) => {
+            cross.addEventListener("click", (e) => {
+                let position = 0;
+                exerciceArray.map((exo) => {
+                    if (exo.pic == e.target.dataset.pic) {
+                        exerciceArray.splice(position, 1);
+                        console.log(exerciceArray);
+                        page.lobby();
+                        this.store();
+                    } else {
+                        position++;
+                    }
+                });
+            });
+        });
+    },
+    reset: function () {
+        reboot.addEventListener("click", () => {
+            exerciceArray = basicArray;
+            page.lobby();
+            utils.store();
+        });
+    },
+    store: function () {
+        localStorage.exercices = JSON.stringify(exerciceArray);
+    },
+    menu: function () {
+        home.addEventListener("click", () => page.lobby());
     },
 };
 
@@ -48,18 +170,29 @@ const page = {
             "<ul>" + mapArray + "</ul>",
             "<button id='start'>Commencer<i class='far fa-play-circle'></i></button>"
         );
+
+        utils.EventMinutes();
+        utils.EventArrow();
+        utils.EventCross();
+        utils.reset();
+        start.addEventListener("click", () => this.routine());
     },
 
     routine: function () {
-        utils.pageContent("Routine", "Exercicepage2", null);
+        const exercice = new Exercice();
+
+        utils.pageContent("Routine", exercice.updateCountdown(), null);
     },
 
     finish: function () {
         utils.pageContent(
             "C'est terminé",
-            "<button  id='start'> Recommencer</button>",
+            "<button  id='start'> Recommencer</button> <br> <button id='home'>Menu</button>",
             "<button id='reboot'>Réinitialiser <i class='far fa-times-circle'></i></button>"
         );
+        start.addEventListener("click", () => this.routine());
+        utils.reset();
+        utils.menu();
     },
 };
 page.lobby();
